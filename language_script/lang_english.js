24548f79145545d8bf530006f1b6e7e2e9d849c8
DK@Macbook <iamjdg11@gmail.com>
2023-05-13 23:18:27 +0900

    first commit

---
+++ language_script/lang_english.js
@@ -0,0 +1,9 @@
+
+// document.documentElement.style.fontFamily =
+//   "'Roboto', sans-serif";
+
+const titleElement = document.getElementById("title");
+const noticeElement = document.getElementById("notice");
+
+titleElement.innerText = "AimPawPaw - AI Consultant for Pet Pals!";
+noticeElement.innerText = " ";
---
+++ language_script/lang_korean.js
@@ -0,0 +1,8 @@
+// document.documentElement.style.fontFamily =
+//   "'Noto Sans KR', sans-serif";
+
+const titleElement = document.getElementById("title");
+const noticeElement = document.getElementById("notice");
+
+titleElement.innerText = "아임포포 - 반려동물 집사님을 위한 AI 상담사";
+noticeElement.innerText = " ";
